33f3f2605a86ddf365dea87f6b9b54dce41b1c01
eguchi <eguchi@asial.co.jp>
2019-03-05 18:26:31 +0900

    #1 分かち書きの粒度を修正

--- app/ChatterBot/app/Services/MecabService.php
+++ app/ChatterBot/app/Services/MecabService.php
@@ -14,10 +14,10 @@ class MecabService
 {
     /** @var WordRepository */
     private $wordRepository;
-
+    
     /** 機械的なテンプレ返答シリーズ */
     private $defaultWords = [
-        'ちょっと何言っているのかわからなかったです...'
+      'ちょっと何言っているのかわからなかったです...'
     ];
     
     /**
@@ -46,16 +46,28 @@ class MecabService
      * @param $words
      * @return Array
      */
-    public function getNotExistWord($words) : Array
+    public function getNotExistWord($words): Array
     {
-        /* ▼3要素ずつに分割
+        /* ▼[[0,1,2], [1,2,3], [2,3,4]...]とindexを一つずつずらした3要素に分割する
          * [
          *  [1,2,3],
          *  [4,5,6],
          *  [7,8]
          * ]
          */
-        $chunk_words = array_chunk($words, 3);
+        $chunk_words = [];
+        $lastIndex = count($words) - 1;
+        for ($i = 0; $i <= $lastIndex; $i++) {
+            $threeLengthWords = [];
+            array_push($threeLengthWords, $words[$i]);
+            if ($i + 1 <= $lastIndex) {
+                array_push($threeLengthWords, $words[$i + 1]);
+            }
+            if ($i + 2 <= $lastIndex) {
+                array_push($threeLengthWords, $words[$i + 2]);
+            }
+            array_push($chunk_words, $threeLengthWords);
+        }
         return array_filter($chunk_words, function ($wordsArray) {
             // 3要素の不足分をnullで穴埋めする。[7,8] -> [7,8, ]となる。
             $padded_words = array_pad($wordsArray, 3, null);
@@ -102,7 +114,7 @@ class MecabService
         }
         return $repl;
     }
-
+    
     /**
      * 該当する返答が得られなかった時にそれっぽい言葉を機械的に返す処理
      * @return String
