30f540b864d01fa9e4cdf06fd7df362f374a578a
eguchi <eguchi@asial.co.jp>
2019-03-08 11:43:29 +0900

    refactor

--- app/ChatterBot/app/Services/MecabService.php
+++ app/ChatterBot/app/Services/MecabService.php
@@ -37,7 +37,7 @@ class MecabService
     public function separateWord(String $word): Array
     {
         // コマンドインジェクション対策。wordをシングルコートでくくって、一つの文字列扱いにする
-        $word = escapeshellarg($word);
+        // $word = escapeshellarg($word);
         $command = "echo ${word} | mecab -E '' -F '%m\n'";
         exec($command, $output, $return_value);
         return $output;
