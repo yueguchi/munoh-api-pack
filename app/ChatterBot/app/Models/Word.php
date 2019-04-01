ba5c5e0723c759bd8a8f8cdd411c73ebd0caebc8
eguchi <eguchi@asial.co.jp>
2019-04-01 18:48:23 +0900

    GSIへのアクセスロールを追加

--- app/ChatterBot/app/Models/Word.php
+++ app/ChatterBot/app/Models/Word.php
@@ -13,9 +13,16 @@ class Word extends Model
     protected $fillable = ['id', 'word1', 'word2', 'word3'];
     
     // GSIの設定。これを有効にしてしまうと、word1絞り込み時にQuery検索になってくれるのは嬉しいが、POST400エラーになる問題...
-//    protected $dynamoDbIndexKeys = [
-//      'word1-index' => [
-//        'hash' => 'word1'
-//      ],
-//    ];
+    // ↑ロールで以下のようにindexを追記して解決
+    /*
+        "Resource": [
+            "arn:aws:dynamodb:ap-northeast-1:リソース番号:table/words",
+            "arn:aws:dynamodb:ap-northeast-1:リソース番号:table/words/index/word1-index"
+        ]
+    */
+    protected $dynamoDbIndexKeys = [
+      'word1-index' => [
+        'hash' => 'word1'
+      ],
+    ];
 }
