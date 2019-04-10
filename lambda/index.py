ba112c853c248505eb3be19f07e0b27c4cb40bff
eguchi <eguchi@asial.co.jp>
2019-04-10 15:19:48 +0900

    lambda更新

--- lambda/index.py
+++ lambda/index.py
@@ -1,6 +1,7 @@
 import boto3
 import requests
 import logging
+import json
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 from requests_aws4auth import AWS4Auth
@@ -18,7 +19,6 @@ url = host + '/' + index + '/' + type + '/'
 headers = { "Content-Type": "application/json" }
 
 def handler(event, context):
-    logger.info('got event{}'.format(event))
     count = 0
     for record in event['Records']:
         # Get the primary key for use as the Elasticsearch ID
@@ -27,7 +27,14 @@ def handler(event, context):
         if record['eventName'] == 'REMOVE':
             r = requests.delete(url + id, auth=awsauth)
         else:
+            # word1 = record['dynamodb']['NewImage']['word1']['S']
+            # word2 = record['dynamodb']['NewImage']['word2']['S']
+            # word3 = record['dynamodb']['NewImage']['word3']['S']
             document = record['dynamodb']['NewImage']
+            logger.info('got NewImage {}'.format(document))
+            logger.info('endpoint: %s' % url + id)
             r = requests.put(url + id, auth=awsauth, json=document, headers=headers)
+            logger.info('result {}'.format(r))
+            logger.info("detail: " + r.text)
         count += 1
     return str(count) + ' records processed.'
