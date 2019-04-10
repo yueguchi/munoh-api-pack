7125fe433f5d96752dd08f0e96a4e05dc0320f97
eguchi <eguchi@asial.co.jp>
2019-04-10 15:32:40 +0900

    dynamodbで範囲検索にしたいため、timestampをU マイクロに変更

--- app/ChatterBot/app/Models/Word.php
+++ app/ChatterBot/app/Models/Word.php
@@ -11,6 +11,8 @@ class Word extends Model
     protected $primaryKey = 'id';
     
     protected $fillable = ['id', 'word1', 'word2', 'word3'];
+    // 自動で作成されるcreated_atとupdated_atをISO型からマイクロ秒にする
+    protected $dateFormat = 'U';
     
     // GSIの設定。これを有効にしてしまうと、word1絞り込み時にQuery検索になってくれるのは嬉しいが、POST400エラーになる問題...
     // ↑ロールで以下のようにindexを追記して解決
